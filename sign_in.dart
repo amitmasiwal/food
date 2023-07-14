31f04a49963ffee865827c8d0810e109cd1dec35
amitmasiwal <masiwala47@gmail.com>
2023-07-14 16:15:44 +0530

    i'm doing a small change in my code i'm doing a small change in my code

---
+++ sign_in.dart
@@ -0,0 +1,30 @@
+import'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget{
+  @override
+  _SignInState createState() => SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+
+  @override
+  Widget build(BuildContext Context){
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+
+          )
+        )
+      ),
+    )
+  }
+
+
+
+
+
+
+}
