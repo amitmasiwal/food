edb3c3f0062048447ed05cf82ef7caf5f16dd4c9
amitmasiwal <masiwala47@gmail.com>
2023-07-14 13:32:50 +0530

    i'm doing a small change in my code i'm doing a small change in my code

--- lib/auth/sign_in.dart
+++ lib/auth/sign_in.dart
@@ -1,5 +1,10 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:flutter/material.dart';
+import 'package:sign_in_button/sign_in_button.dart';
 class SignIn extends StatelessWidget{
+  const SignIn({super.key});
+
   @override
 Widget build(BuildContext context) {
     return  Scaffold(
@@ -15,14 +20,38 @@ body: Container(
   child: Column(
     mainAxisAlignment: MainAxisAlignment.center,
     children: [
-      Container(
+      SizedBox(
         height: 400,
         width: double.infinity,
-        color: Colors.red,
         child: Column(
           children: [
-            Text('Sign in to continue'),
-            Text('')
+            const Text('Sign in to continue'),
+            Text('Sabzi', style:  TextStyle(fontSize: 50,
+              color: Colors.white,
+              shadows: [
+                BoxShadow(
+                  blurRadius: 5,
+                  color: Colors.green.shade900,
+                  offset: const Offset(3,3),
+                )
+              ]
+            ),),
+            SignInButton(
+              Buttons.google,
+              text:"Sign Up With Google",
+              onPressed: (){},
+            ),
+             SignInButton(
+              Buttons.apple, text: "Sign Up With Apple",
+               onPressed: (){},
+),
+            SignInButton(
+              Buttons.facebook,
+              text: "Sign Up With Facebook",
+              onPressed: (){},
+            ),
+            Text('by signing in you are agreeing to our', style: TextStyle(color: Colors.grey[800]),),
+            Text('Terms and our Privacy Policy', style: TextStyle(color: Colors.grey[800]),),
           ],
         )
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
+// ignore_for_file: non_constant_identifier_names, depend_on_referenced_packages
+
 import 'package:flutter/material.dart';
-import 'package:food/auth/sign_in.dart';
+
+import 'auth/sign_in.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp( const MyApp());
 }
 
 class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
   //This Widget is the root of your application
 
   Widget Build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: SignIn(),
     );
 
